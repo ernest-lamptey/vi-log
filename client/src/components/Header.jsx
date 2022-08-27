4da2f3ea9f5988b1f504617b87e6050d077a486f
PrinceQuarshie <prince.quarshie@amalitech.org>
2022-08-27 23:21:43 +0000

    header refactoring

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import "../styles/Header.scss";
-// import { MdDashboard } from 'react-icons';
+
 import { MdDashboard } from "react-icons/md";
 
 
 function Header() {
+   
   return (
       <nav className='nav'>
           {/* <a href="#" className='nav_brand'>herdoy</a> */}
-          <ul className='nav_menu'>
-              <li className='nav_item'><a href="#" className='nav_link'>Dashboard</a></li>
+          <ul className="nav_menu">
+              <li className='nav_item'><a href="#" className='nav_link'><MdDashboard /></a></li>
           </ul>
          <div className="nav_toggler">
              <div className="line1"></div>
