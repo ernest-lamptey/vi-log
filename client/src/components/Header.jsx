833791e23fbf970d51885226131989b47b96f879
PrinceQuarshie <prince.quarshie@amalitech.org>
2022-08-27 22:58:48 +0000

    header toggle styling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,7 +7,7 @@ import { MdDashboard } from "react-icons/md";
 function Header() {
   return (
       <nav className='nav'>
-          <a href="#" className='nav_brand'>herdoy</a>
+          {/* <a href="#" className='nav_brand'>herdoy</a> */}
           <ul className='nav_menu'>
               <li className='nav_item'><a href="#" className='nav_link'>Dashboard</a></li>
           </ul>
