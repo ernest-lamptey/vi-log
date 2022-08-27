3ccbe4991c050062e1371acd93b25cb68efafa69
ernest-lamptey <99683999+ernest-lamptey@users.noreply.github.com>
2022-08-27 22:25:18 +0000

    add email feature

--- server/visitors/visitorService.js
+++ server/visitors/visitorService.js
@@ -1,4 +1,25 @@
 const pool = require('../db');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+// setup for email
+let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_APP_PASS
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
+})
+// email sending options and message
+let mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: "1ernestlamptey@gmail.com",
+    subject: "Testing",
+    text: "First email sent with nodemailer"
+}
 
 const addVisitor = (body) => {
     // add visitor details to visits table in db
@@ -9,12 +30,21 @@ const addVisitor = (body) => {
         VALUES ($1, $2, $3, $4, $5, $6, $7)`, 
         [body.name, body.phone, body.email, body.company, body.purpose, body.host_id, body.photo_url]
     )
-    .then((res) => { console.log("Login details added to database") })
+    .then((res) => {
+        console.log("Login details added to database");
+        // sending the email
+        transporter.sendMail(mailOptions, function(err, success) {
+            if (err) {
+                console.error(err)
+            } else {
+                console.log("Email sent successfully")
+            }
+        })
+    })
     .catch(e => {
         console.error(e.message)
         return {status: e?.status || 500, message: e.message}
     })
-
 }
 
 //import get all employees from admin module.
