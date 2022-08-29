0ad3839a1dde1a7c9802751739f015798d35be73
PrinceQuarshie <prince.quarshie@amalitech.org>
2022-08-29 15:21:45 +0000

    react routing to admin

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,29 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 import "../styles/Navbar.scss";
 import { Link } from "react-router-dom";
 
 import { MdDashboard } from "react-icons/md";
 
 function Navbar() {
-    const [ isMobile, setIsMobile ] = useState(false);
+
   
  
   return (
     <nav className="navbar">
-      <ul className={isMobile ? "nav-links-mobile" : "nav-links"}>
+      <ul
+        className="nav-links">
         <Link to="/dashboard" className="dashboard">
           <li>
             <MdDashboard />
           </li>
         </Link>
       </ul>
-      <button className="mobile-menu-icon">
-        {isMobile ? (
-          <i className="fas fa-times"></i>
-        ) : (
-          <i className="fas fa-bars"></i>
-        )}
-      </button>
     </nav>
   );
 }
