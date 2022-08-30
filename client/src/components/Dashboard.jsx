75f4f445fce9675d50f7f0d7aa3150a0e723b9c2
PrinceQuarshie <prince.quarshie@amalitech.org>
2022-08-30 12:19:12 +0000

    dashboard styling

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
-import "../styles/Dashboard.scss"
-import logo from "../assets/Amalitech-Ghana.png"
+import React from "react";
+import "../styles/Dashboard.scss";
+import logo from "../assets/Amalitech-Ghana.png";
 function Dashboard() {
   return (
-    <div className='body'>
-    <div className='container'>
-      <nav>
-        <ul>
-          <li><a href="#" className='logo'>
-             <img src={logo} alt="/" />
-             <span className='nav-item'>Admin</span>
-            </a></li>
-        </ul>
-      </nav>
-        
-        
-          
-        </div>
-    </div>
-  )
+    <section>
+      <div className="container">
+        <nav className="header">
+          <ul>
+            <li>
+              <a href="#" className="logo">
+                <img src={logo} alt="/" />
+                <span className="nav-item">Admin</span>
+                <li><a href="#">
+                  <i className="fas fa-menorah"></i>
+                  <span className="nav-item">Dashboard</span>
+                  </a></li>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </section>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
