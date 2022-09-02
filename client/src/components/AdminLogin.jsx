7da0bd45ddc193af96f8b8d36881e1be415adbe4
PrinceQuarshie <prince.quarshie@amalitech.org>
2022-09-02 14:42:42 +0000

    admin form styling

--- client/src/components/AdminLogin.jsx
+++ client/src/components/AdminLogin.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from 'react';
+import "../styles/AdminLogin.scss";
 
 function AdminLogin() {
   return (
+<div className='main-wrap'>
     <div className="wrapper">
         <h1>Sign in</h1>
         <form>
@@ -10,10 +12,12 @@ function AdminLogin() {
           <input type="text" name="" value="LOGIN" />
         </form>
         <div className='bottom-text'>
-
+           <input type="checkbox" name='remember' checked="checked"/>
+           <a href="#">Forgot Password</a>
         </div>
 
     </div>
+</div>
   )
 }
 
