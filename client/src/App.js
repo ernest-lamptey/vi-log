c95d3540976ee0b6db1f6661802d0993e3c63830
PrinceQuarshie <prince.quarshie@amalitech.org>
2022-09-02 10:38:41 +0000

    qrcode scanner

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,27 @@
+import React from "react";
 import "./App.css";
 import User from "./components/User";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
+import { ScanQrCode } from "./components/ScanQrCode";
 
 function App() {
   return (
     <Router>
-        <Navbar />
-        <Switch>
-          <Route path="/" component={User} exact>
-            <User />
-          </Route>
-          
-          <Route component={Dashboard} path="/dashboard">
-            <Dashboard />
-          </Route>
-          
+      <Navbar />
+      <Switch>
+        <Route path="/" component={User} exact>
+          <User />
+        </Route>
+
+        <Route path="/dashboard" component={Dashboard}>
+          <Dashboard />
+        </Route>
+
+        <Route path="/scanqrcode" component={ScanQrCode}>
+          <ScanQrCode />
+        </Route>
       </Switch>
     </Router>
   );
---
+++ client/src/components/ScanQrCode.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import QrReader from "react-qr-scanner";
+import "../styles/ScanQrCode.scss";
+
+export class ScanQrCode extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      delay: 100,
+      result: "No result",
+    };
+
+    this.handleScan = this.handleScan.bind(this);
+  }
+  handleScan(data) {
+    this.setState({
+      result: data,
+    });
+  }
+  handleError(err) {
+    console.error(err);
+  }
+  render() {
+    const previewStyle = {
+      height: 240,
+      width: 320,
+      marginLeft: 500,
+      marginRight: 500,
+      marginTop: 100,
+
+    };
+
+    return (
+      <div>
+        <QrReader
+          delay={this.state.delay}
+          style={previewStyle}
+          onError={this.handleError}
+          onScan={this.handleScan}
+        />
+        <p>{this.state.result}</p>
+      </div>
+    );
+  }
+}
