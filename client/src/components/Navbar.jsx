94b58466233d6b79955693ebfbbb5a5833dc002f
ernest-lamptey <99683999+ernest-lamptey@users.noreply.github.com>
2022-09-03 12:26:51 +0000

    add homepage

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import "../styles/Navbar.scss";
 import { Link } from "react-router-dom";
 import Logo from "./Logo"
-import { MdDashboard } from "react-icons/md";
-import { BsFillCameraFill } from "react-icons/bs";
+import { MdDashboard, MdOutlineQrCodeScanner } from "react-icons/md";
+// import { BsFillCameraFill } from "react-icons/bs";
+
 
 function Navbar() {
 
@@ -12,14 +13,14 @@ function Navbar() {
   return (
     <nav className="navbar">
       <ul className="nav-links">
-        <Link to="/home" className="vi-logo">
+        <Link to="/" className="vi-logo">
           <li>
             <Logo />
           </li>
         </Link>
         <Link to="/scanqrcode" className="scanqrcode">
           <li>
-            <BsFillCameraFill />
+            <MdOutlineQrCodeScanner />
           </li>
         </Link>
         <Link to="/dashboard" className="dashboard">
