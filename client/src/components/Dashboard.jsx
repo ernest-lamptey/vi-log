efd3904b003162fd49923d5d78766ce5bfa1a351
PrinceQuarshie <prince.quarshie@amalitech.org>
2022-09-05 12:47:28 +0000

    dashbaord cards stylying

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,10 +1,5 @@
 import React from "react";
 import "../styles/Dashboard.scss";
-// import logo from "../assets/Amalitech-Ghana.png";
-// import { MdDashboard } from "react-icons/md";
-// import { TbReportSearch } from "react-icons/tb";
-// import { IoMdContact } from "react-icons/io";
-// import Cards from "./Cards";
 import { MdLogout } from "react-icons/md";
 import { AiOutlineHome, AiOutlineMail, AiOutlinePieChart } from "react-icons/ai";
 import { RiAdminLine } from "react-icons/ri";
@@ -91,59 +86,3 @@ function Dashboard() {
 }
 
 export default Dashboard;
-
-
-
-
-
-//  <section>
-//       <div className="wrapper">
-//         <nav className="header">
-//           <ul>
-//             <li>
-//               <a href="#" className="logo">
-//                 <img src={logo} alt="/" />
-//               </a>
-//             </li>
-
-//             <li>
-//               <a href="#">
-//                 <MdDashboard className="fas" />
-//                 <span className="nav-item">Dashboard</span>
-//               </a>
-//             </li>
-
-//             <li>
-//               <a href="#">
-//                 <IoMdContact className="fas" />
-//                 <span className="nav-item">Contact</span>
-//               </a>
-//             </li>
-
-//             <li>
-//               <a href="#">
-//                 <TbReportSearch className="fas" />
-//                 <span className="nav-item">Report</span>
-//               </a>
-//             </li>
-//             <li className="logout">
-//               <a href="#">
-//                 <MdLogout className="fas" />
-//                 <span className="nav-item">Logout</span>
-//               </a>
-//             </li>
-//           </ul>
-//         </nav>
-
-//         <section className="main">
-//           <div className="main-top">
-//             <h1>Total Visitors</h1>
-//             <BsFillPersonLinesFill className="person" />
-//           </div>
-
-//           <Cards />
-//           <Table />
-          
-//         </section>
-//       </div>
-//     </section>
