d7a3f37648209a7847f047de9c0981ffd8480f88
PrinceQuarshie <prince.quarshie@amalitech.org>
2022-09-05 14:28:44 +0000

    dashbaord info section styling

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -86,28 +86,35 @@ function Dashboard() {
           </div>
         </div>
          
-       <div className="name">
-         <div className="name-details">
+       <div className="info">
+         <div className="info-details">
            <ul className="details">
              <li className="topic">Date</li>
-             <li><a href="@">01 - 05 2022</a></li>
-             <li><a href="@">05 - 10 2022</a></li>
-             <li><a href="@">01 - 01 2022</a></li>
-             <li><a href="@">27 - 08 2022</a></li>
+             <li><a href="#"></a></li>
+             <li><a href="#">05 - 10 - 2022</a></li>
+             <li><a href="#">01 - 01 - 2022</a></li>
+             <li><a href="#">27 - 08 - 2022</a></li>
            </ul>
            <ul className="details">
-             <li className="topic">Date</li>
-             <li><a href="@">01 - 05 2022</a></li>
-             <li><a href="@">05 - 10 2022</a></li>
-             <li><a href="@">01 - 01 2022</a></li>
-             <li><a href="@">27 - 08 2022</a></li>
+             <li className="topic">Name</li>
+             <li><a href="#">Prince Quarshie</a></li>
+             <li><a href="#">Michael Andoh</a></li>
+             <li><a href="#">Ernest Lamptey</a></li>
+             <li><a href="#">Denis Wrights</a></li>
            </ul>
            <ul className="details">
-             <li className="topic">Date</li>
-             <li><a href="@">01 - 05 2022</a></li>
-             <li><a href="@">05 - 10 2022</a></li>
-             <li><a href="@">01 - 01 2022</a></li>
-             <li><a href="@">27 - 08 2022</a></li>
+             <li className="topic">Status</li>
+             <li><a href="#">Done</a></li>
+             <li><a href="#">Pending</a></li>
+             <li><a href="#">Done</a></li>
+             <li><a href="#">Pending</a></li>
+           </ul>
+           <ul className="details">
+             <li className="topic">Company</li>
+             <li><a href="#">Apple Inc</a></li>
+             <li><a href="#">Microsoft Corp</a></li>
+             <li><a href="#">Amazon.com Inc</a></li>
+             <li><a href="#">Saudi Aramco</a></li>
            </ul>
          </div>
          </div>  
