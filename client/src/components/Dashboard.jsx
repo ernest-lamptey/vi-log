0840879a631b23a3115e556936a0df05863d0df2
ernest-lamptey <99683999+ernest-lamptey@users.noreply.github.com>
2022-09-11 16:58:39 +0000

    add routes on dashboard

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/Dashboard.scss";
-import { BrowserRouter as Router, Route, Switch, useRouteMatch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, useRouteMatch, NavLink } from "react-router-dom";
 import { MdLogout } from "react-icons/md";
 import { AiOutlineHome, AiOutlineMail, AiOutlinePieChart } from "react-icons/ai";
 import { RiAdminLine } from "react-icons/ri";
@@ -9,16 +9,29 @@ import Reports from "./Reports"
 import DailyStats from "./DailyStats";
 
 function Dashboard() {
+  const { path } = useRouteMatch()
+  console.log(path)
   return (
     <Router>
+        <ul className="dashboard-links">
+          <NavLink to="/dashboard">
+            <li>Dashboard</li>
+          </NavLink>
+          <NavLink to="/dashboard/reports">
+            <li>Reports</li>
+          </NavLink>
+          <NavLink to="/dashboard/manage">
+            <li>Manage</li>
+          </NavLink>
+        </ul>
       <Switch>
-        <Route path="/" component={DailyStats}>
+        <Route exact path={`${path}`} component={DailyStats}>
           <DailyStats />
         </Route>
 
-        {/* <Route path="/" component={Reports}>
+        <Route exact path={`${path}/reports`} component={Reports}>
           <Reports />
-        </Route> */}
+        </Route>
 
       </Switch>
     </Router>
