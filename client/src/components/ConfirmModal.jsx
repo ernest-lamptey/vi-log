b7376476bd2148bfb73be241a51b97d2411c3945
ernest-lamptey <99683999+ernest-lamptey@users.noreply.github.com>
2022-09-12 13:04:43 +0000

    delete employee troubleshoot 4 - fixed delete

--- client/src/components/ConfirmModal.jsx
+++ client/src/components/ConfirmModal.jsx
@@ -4,18 +4,6 @@ import '../styles/modal.scss'
 const ConfirmModal = () => {
   return (
     <>
-      <form className='edit-modal'>
-        <label htmlFor="f_name">First Name</label>
-        <input type="text" />
-        <label htmlFor="l_name">Last Name</label>
-        <input type="text" />
-        <label htmlFor="department">Department</label>
-        <input type="text" />
-        <label htmlFor="email">Email</label>
-        <input type="text" />
-        <label htmlFor="f_name">Phone</label>
-        <input type="text" />
-      </form>
     </>
   )
 }
